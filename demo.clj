5785a43d84939ea3b6d931979af2cc3ae96d4326
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2011-12-14 23:15:00 +0900

    Updated demo

--- demo.clj
+++ demo.clj
@@ -5,13 +5,9 @@
   *command-line-args*
   (new-argument-parser
    {:prog "prog", :description "Process some integers."}
-   (add-argument "integers" {:metavar "N"
-                             :type Long
-                             :nargs "+"
+   (add-argument "integers" {:metavar "N", :type Integer, :nargs "+"
                              :help "an integer for the accumulator"})
-   (add-argument "--sum" {:dest "accumulate"
-                          :action :store-const
-                          :const +
-                          :default max
+   (add-argument "--sum" {:dest "accumulate", :action :store-const,
+                          :const +, :default max
                           :help "sum the integers (default: find the max)"}))))
 (println (apply (args :accumulate) (args :integers)))
